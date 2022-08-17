5d4b9b92e1cc612a4a3447206eb2ecce5bbaf95c
yakam115 <111244560+yakam115@users.noreply.github.com>
2022-08-17 15:54:33 +0100

    Add files via upload

---
+++ data/campusbgtemodifer_2.js
@@ -0,0 +1,11 @@
+var json_campusbgtemodifer_2 = {
+"type": "FeatureCollection",
+"name": "campusbgtemodifer_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id_CAMPUS": "0", "Nom campus": "Ecole des Ingenieurs ISTIC-E2I MADJA", "latitude": "670185.071", "longitude": "570081.55" }, "geometry": { "type": "Point", "coordinates": [ 10.535399601991399, 5.15571285088589 ] } },
+{ "type": "Feature", "properties": { "id_CAMPUS": "1", "Nom campus": "UDM BANEKANE 1", "latitude": "676186.124", "longitude": "567806.747" }, "geometry": { "type": "Point", "coordinates": [ 10.589475832163592, 5.135008606666358 ] } },
+{ "type": "Feature", "properties": { "id_CAMPUS": "2", "Nom campus": "UDM MFETUM", "latitude": "668987.931", "longitude": "567355.258" }, "geometry": { "type": "Point", "coordinates": [ 10.524542999131269, 5.131083996540815 ] } },
+{ "type": "Feature", "properties": { "id_CAMPUS": "3", "Nom campus": "ISTEC", "latitude": "670330", "longitude": "567894" }, "geometry": { "type": "Point", "coordinates": [ 10.536659350232334, 5.135927004646946 ] } }
+]
+}
---
+++ data/hopitauxdebgtemodifier_5.js
@@ -0,0 +1,15 @@
+var json_hopitauxdebgtemodifier_5 = {
+"type": "FeatureCollection",
+"name": "hopitauxdebgtemodifier_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "MATRICULE": "1", "longitude": "10,532203", "latitude": "5,136951", "altitude": "1302,191528", "descriptio": "CS DERCAU DES ANGES(BGTE)", "X": "669835.651", "Y": "568006.047" }, "geometry": { "type": "Point", "coordinates": [ 10.532203002028465, 5.136951002886009 ] } },
+{ "type": "Feature", "properties": { "MATRICULE": "2", "longitude": "10,53353", "latitude": "5,142264", "altitude": "1328,436157", "descriptio": "CSI DE BGTE(QTR6)", "X": "669961.743", "Y": "568593.854" }, "geometry": { "type": "Point", "coordinates": [ 10.533353001881965, 5.142263999912951 ] } },
+{ "type": "Feature", "properties": { "MATRICULE": "3", "longitude": "10,520421", "latitude": "5,141692", "altitude": "1353,577515", "descriptio": "DS BGTE", "X": "668528.134", "Y": "568527.178" }, "geometry": { "type": "Point", "coordinates": [ 10.520420997296789, 5.141691997795202 ] } },
+{ "type": "Feature", "properties": { "MATRICULE": "4", "longitude": "10,519876", "latitude": "5,141567", "altitude": "1359,864624", "descriptio": "H�PITAL DE DISTRICT DE BGTE", "X": "668467.744", "Y": "568513.212" }, "geometry": { "type": "Point", "coordinates": [ 10.519876003145788, 5.141566996810747 ] } },
+{ "type": "Feature", "properties": { "MATRICULE": "5", "longitude": "10,51157", "latitude": "5,159621", "altitude": "1446,193481", "descriptio": "CS LES MERVEILLES BGTE", "X": "667542.139", "Y": "570507.388" }, "geometry": { "type": "Point", "coordinates": [ 10.511569995640544, 5.159621002521055 ] } },
+{ "type": "Feature", "properties": { "MATRICULE": "6", "longitude": "10,520056", "latitude": "5,153947", "altitude": "1371,534058", "descriptio": "CSI (AD LUCAM)", "X": "668484.441", "Y": "569882.21" }, "geometry": { "type": "Point", "coordinates": [ 10.520056003343825, 5.153947003851055 ] } },
+{ "type": "Feature", "properties": { "MATRICULE": "7", "longitude": "10,516459", "latitude": "5,135633", "altitude": "1325,22229", "descriptio": "CS PRIVE DE NETA", "X": "668090.461", "Y": "567856.147" }, "geometry": { "type": "Point", "coordinates": [ 10.516459000488247, 5.135633002039422 ] } },
+{ "type": "Feature", "properties": { "MATRICULE": "8", "longitude": "10,589178", "latitude": "5,132308", "altitude": "1312,360352", "descriptio": "CLINIQUE UDM", "X": "676153.844", "Y": "567508.029" }, "geometry": { "type": "Point", "coordinates": [ 10.589178003270233, 5.132307999730956 ] } }
+]
+}
